51f3e11e21082ea9e297b89c811eac1dc6431568
Kamau Vassall <kamau.vassall@gmail.com>
2020-01-31 21:50:47 -0600

    Adding in Basic scripts

---
+++ Rods Away/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerController : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+}
