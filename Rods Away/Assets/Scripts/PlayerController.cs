ffc90596c4eb07aa99c94db227df47b92db81b61
Travis <ponasity@live.com>
2020-01-31 23:44:53 -0600

    im staging this shit

--- Rods Away/Assets/Scripts/PlayerController.cs
+++ Rods Away/Assets/Scripts/PlayerController.cs
@@ -7,9 +7,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
+    public GameObject meleeCollider;
+    public float speed = 5;
 
     private void Awake()
     {
         boxCollider = GetComponent<BoxCollider2D>();
     }
+
+    protected void Update()
+    {
+        float move = Input.GetAxis("Horizontal") * speed;
+
+        transform.Translate(Vector3.right * (Time.deltaTime * move), Space.World);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.LogFormat("Fire1");
+            meleeCollider.GetComponent<BoxCollider2D>().enabled = true;
+            meleeCollider.GetComponent<Renderer>().enabled = true;
+        }
+        //Instantiate(projectile, transform.position, transform.rotation);
+        
+        //Debug.LogFormat("move:{0}", move);
+    }
 }
+        
