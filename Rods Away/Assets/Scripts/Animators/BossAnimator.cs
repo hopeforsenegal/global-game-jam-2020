e129d2d94d3c0bd9fd9f0646405da6f838d80e8b
Kamau Vassall <kamau.vassall@gmail.com>
2020-02-02 05:56:14 -0600

    Adding in the boss animator

---
+++ Rods Away/Assets/Scripts/Animators/BossAnimator.cs
@@ -0,0 +1,89 @@
+﻿
+using System;
+using MoonlitSystem.Animators;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(StandardAnimator))]
+public class BossAnimator : MonoBehaviour
+{
+    #region Enums and Constants
+
+    #endregion
+
+    #region Events
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Inspectables
+
+    #endregion
+
+    #region Private Member Variables
+
+    private StandardAnimator m_StandardAnimator;
+
+    #endregion
+
+    #region Monobehaviours
+
+    private void Awake()
+    {
+        m_StandardAnimator = GetComponent<StandardAnimator>();
+
+        Debug.Assert(m_StandardAnimator != null);
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void Idle()
+    {
+        //Debug.LogFormat("Idle");
+        m_StandardAnimator.Loop(string.Empty, "idle");
+    }
+
+    public void Run()
+    {
+        //Debug.LogFormat("Run");
+        m_StandardAnimator.Loop(string.Empty, "run1");
+    }
+
+    public void Die(Action onDieAnimationComplete)
+    {
+        Debug.LogFormat("Die");
+        m_StandardAnimator.Play(string.Empty, "die", string.Empty, ()=> {
+            m_StandardAnimator.Clear();
+            onDieAnimationComplete?.Invoke();
+        });
+    }
+
+    public void Jump()
+    {
+        Debug.LogFormat("Jump");
+        m_StandardAnimator.Play(string.Empty, "jump", "idle");
+    }
+
+    public void Melee()
+    {
+        Debug.LogFormat("Melee");
+        m_StandardAnimator.Play(string.Empty, "blast", "idle");
+    }
+
+    public void Range()
+    {
+        Debug.LogFormat("Range");
+        m_StandardAnimator.Play(string.Empty, "arms_shoot", "idle");
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    #endregion
+}
---
+++ Rods Away/Assets/Scripts/Animators/BossRenderer.cs
@@ -0,0 +1,133 @@
+﻿
+using System;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class BossRenderer : MonoBehaviour
+{
+    #region Enums and Constants
+
+    #endregion
+
+    #region Events
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Inspectables
+
+    [SerializeField]
+    private float idleTimer = 0.5f;
+
+    [SerializeField]
+    private BossController m_BossController = default;
+
+    [SerializeField]
+    private BossAnimator m_BossAnimator = default;
+
+    #endregion
+
+    #region Private Member Variables
+
+    private float m_CurrentTimer;
+    private bool m_LastDirection;
+    private bool m_Dead;
+
+    #endregion
+
+    #region Monobehaviours
+
+    protected void Start()
+    {
+        Debug.Assert(m_BossController != null, "m_BossController not set");
+        Debug.Assert(m_BossAnimator != null, "m_BossAnimator not set");
+
+        m_BossAnimator.Idle();
+
+        m_CurrentTimer = Time.time;
+
+        m_BossController.AttackEvent += OnAttack;
+        m_BossController.DieEvent += OnDie;
+    }
+
+    protected void OnDestroy()
+    {
+        m_BossController.AttackEvent -= OnAttack;
+        m_BossController.DieEvent -= OnDie;
+    }
+
+    protected void Update()
+    {
+        if (m_Dead)
+            return;
+
+        if (m_CurrentTimer + idleTimer <= Time.time) {
+            m_CurrentTimer = Time.time;
+            m_BossAnimator.Idle();
+            return;
+        }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    #endregion
+
+    #region Private Methods
+
+    private void OnAttack(BossController.AttackPattern attackPattern)
+    {
+        if (attackPattern == BossController.AttackPattern.Melee) {
+            m_BossAnimator.Melee();
+            m_CurrentTimer = Time.time;
+        } else if (attackPattern == BossController.AttackPattern.Projectile) {
+            m_BossAnimator.Range();
+            m_CurrentTimer = Time.time;
+        }
+    }
+
+    private void OnDie()
+    {
+        m_Dead = true;
+        OnDieAction(NotifyGameControllerDeadDead);
+    }
+
+    private void OnRespawn()
+    {
+        m_Dead = false;
+        m_BossAnimator.Idle();
+    }
+
+    private void NotifyGameControllerDeadDead()
+    {
+        if (GameController.TryGetInstance(out GameController gameController)) {
+            gameController.NotifyPlayerWasDefeated();
+        }
+    }
+
+    private void OnDieAction(Action onDieAnimationComplete)
+    {
+        m_BossAnimator.Die(onDieAnimationComplete);
+        m_CurrentTimer = Time.time;
+    }
+
+    private void OnJump()
+    {
+        m_BossAnimator.Jump();
+        m_CurrentTimer = Time.time;
+    }
+
+    private void OnPowerUp(Vector3 location)
+    {
+    }
+
+    private void OnDash()
+    {
+    }
+
+    #endregion
+}
